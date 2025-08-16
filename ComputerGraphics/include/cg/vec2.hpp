ebd4f7c695cb92920f20854dc1fe84924cd24ed2
Yusuf Baha Kama <m_ybaha@hotmail.com>
2025-08-16 19:23:47 +0300

    İmplemented basic vec2 and vec3 classes

--- ComputerGraphics/include/cg/vec2.hpp
+++ ComputerGraphics/include/cg/vec2.hpp
@@ -1,1 +1,17 @@
+#pragma once
 #include "Export.h"
+
+namespace cg {
+
+	class CG_API Vec2 {
+	public:
+		float x, y;
+
+		Vec2();
+		Vec2(float x, float y);
+
+		float length() const;
+
+		Vec2 operator+(const Vec2& other) const;
+	};
+}
